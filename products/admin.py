383ecd9bcfc04914b6fdb19b422f17bfd6e57a21
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-14 13:19:49 +0000

    Create category model and admin #21

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Category model.
+
+    Defines the display fields for the Category admin panel.
+    """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """
+    Model representing a category.
+
+    Defines a category with a name and an optional friendly name.
+    """
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
