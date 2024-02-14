63de0ef62b26636183761fb11ee727c29df04e7c
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-14 11:32:28 +0000

    Include home app in installed apps #20

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def home(request):
     """ A view to return the home page """
 
-    return render(request, 'home/home.html')
+    return render(request, 'home/index.html')
