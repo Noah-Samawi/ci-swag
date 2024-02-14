14fd8feb776b6b5a7d14c98c242b4257a00d2498
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-14 15:22:10 +0000

    Add admin registration for Program and MOdule models #11

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+"Program Admin Models"
 
-# Register your models here.
+from django.contrib import admin
+from .models import Program, Module
+
+
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+
+admin.site.register(Program, ProgramAdmin)
+admin.site.register(Module)
