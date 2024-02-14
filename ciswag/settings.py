5b69ab5e14c31e6fdcb0b6b6e56953177b5cb458
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-14 12:43:39 +0000

    Create custom storages for aws

--- ciswag/settings.py
+++ ciswag/settings.py
@@ -176,3 +176,10 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
+# STRIPE 
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+STRIPE_CURRENCY = 'usd'
