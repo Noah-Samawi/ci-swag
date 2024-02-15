3d308a7758ef199bccbab28bc76f607167913d00
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-15 15:55:54 +0000

    Add checkout urls #33

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,6 @@
+"Checkout Views"
+
+from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
--- ciswag/urls.py
+++ ciswag/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('programs/', include('programs.urls')),
     path('cart/', include('cart.urls')),
     path('profile/', include('profiles.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
