84904c6274ca02e43671f453c2dbca466b29a4e9
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-15 10:11:17 +0000

    Create program template, views and urls, link all together #26

--- programs/views.py
+++ programs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Program
+
+
 
 # Create your views here.
+def all_programs(request):
+    """ A view to show all programs, including sorting and search queries """
+    programs = Program.objects.all()
+
+    context = {
+        'programs': programs,
+
+    }
+
+    return render(request, 'programs/programs.html', context)
