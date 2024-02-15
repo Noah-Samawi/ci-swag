840bb3eeb7b015d1d204050a90a4fd8bac5fd26d
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-15 15:53:35 +0000

    Create checkout template and view #34

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class CheckoutView(View):
+    template_name = 'checkout/checkout.html'
+
+     
+    def get(self, request, *args, **kwargs):  
+
+        return render(request, self.template_name)
+
+
+   
