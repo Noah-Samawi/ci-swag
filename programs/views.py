46b15b8922deccb76dca48e4d0b8b8b5e2ed8408
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-15 10:30:20 +0000

    Implement search/filter functionality on program page #25

--- programs/views.py
+++ programs/views.py
@@ -3,6 +3,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Program
 
+from products.utils import filter_and_sort_products
+
+
 
 
 # Create your views here.
@@ -10,12 +13,21 @@ def all_programs(request):
     """ A view to show all programs, including sorting and search queries """
     programs = Program.objects.all()
 
-    context = {
-        'programs': programs,
+    filtered_products, query, categories, sort, direction, category = \
+        filter_and_sort_products(products=programs, request=request)
 
+    context = {
+        'programs': filtered_products,
+        'search_term': query,
+        'current_categories': categories,
+        'sort': sort,
+        'direction': direction,
+        'query': query,
+        'category': category
     }
 
     return render(request, 'programs/programs.html', context)
+
 
 
 def program_detail(request, program_id):
