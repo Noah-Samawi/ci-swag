ed563915aef61cb881afb163d7ebee7d1a2c2997
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-15 16:02:43 +0000

    Create Order model based on database plan #14

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+"""Checkout Models"""
 
-# Create your models here.
+import uuid
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
+
+
+from profiles.models import UserProfile
+
+
+
+
+class Order(models.Model):
+    """
+    Model representing an order placed by a customer
+    """
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = PhoneNumberField(blank=True, null=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False, default='IE')
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,3 @@ class CheckoutView(View):
     def get(self, request, *args, **kwargs):  
 
         return render(request, self.template_name)
-
-
-   
