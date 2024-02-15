7920987a8c2f135f278882dd013b2a31ef764375
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-15 13:15:35 +0000

    Implement update cart view, link and function

--- cart/views.py
+++ cart/views.py
@@ -32,6 +32,22 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+def update_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
+
 
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
