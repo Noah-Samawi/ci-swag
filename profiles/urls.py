84d436dd11b61e73db5639e4a4958f5feb4763ca
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-20 09:32:30 +0000

    Link order history item to checkout success page #44

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProfileView.as_view(), name='profile'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
     path('subscriptions', views.SubscriptionsView.as_view(), name='subscriptions'),
     path('remove_subscription',views.remove_subscription, name='remove_subscription'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Subscription, UserProfile
 from .forms import UserProfileForm, UpdateUserForm
 
+from checkout.models import Order
 
 
 class ProfileView(LoginRequiredMixin, View):
@@ -131,3 +132,20 @@ def remove_subscription(request):
     except Exception as e:
         messages.error(request, f'Error unsubscribing your membership: {e}')
         return HttpResponse(status=500)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
