9d2524d7c0e4ec1a7cde301824715fb5b9572e3a
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-26 06:51:53 +0000

    Improve accessibility on hero page #6

--- profiles/admin.py
+++ profiles/admin.py
@@ -32,7 +32,7 @@ class UserAdmin(admin.ModelAdmin):
     """
 
     model = User
-    fields = ("username", "first_name", "last_name", "email",)
+    fields = ("username", "first_name", "last_name", "email", 'is_superuser')
     inlines = [ProfileInline]
 
 
