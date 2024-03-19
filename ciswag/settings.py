978da753568ff9b73e282251420db9c9af1fd152
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-03-19 11:06:00 +0000

    Update debug to false

--- ciswag/settings.py
+++ ciswag/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ci-swag-e9f8de0bed4b.herokuapp.com',
                 'https://ci-swag-e9f8de0bed4b.herokuapp.com/' ,'localhost']
